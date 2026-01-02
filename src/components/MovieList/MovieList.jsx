e8c5604f6277b3f8c5cbac5f9f0569ab4609faa8
Mukola <vmv.vynnychuk98@gmail.com>
2026-01-02 16:41:54 +0200

    sol

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,11 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
+import moviesFromServer from '../../api/movies.json';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = () => (
+  <div className="movies">
+    {moviesFromServer.map(movie => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    ))}
+  </div>
+);
